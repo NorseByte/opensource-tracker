833fd897a87f7e15e2f138276bc76189785bfb61
suxSx <marcuscrazy@gmail.com>
2019-10-11 13:44:44 +0200

    Change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="optracker",
-    version="1.0.13",
+    version="1.0.14",
     description=('scrapes medias, likes, followers from social media. Organize them in a database for more deeper analyze.'),
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
