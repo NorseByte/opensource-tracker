292647e2b3e2441412731996d3df1bdfdc1e00b1
suxSx <marcuscrazy@gmail.com>
2019-11-02 14:01:29 +0100

    Update init

--- optracker/igramscraper/exception/__init__.py
+++
@@ -1,5 +0,0 @@
-from igramscraper.exception.instagram_exception import InstagramException
-from igramscraper.exception.instagram_not_found_exception import InstagramNotFoundException
-from igramscraper.exception.instagram_auth_exception import InstagramAuthException
-
-__all__ = ["InstagramException", "InstagramNotFoundException", "InstagramAuthException"]
--- optracker/igramscraper/two_step_verification/__init__.py
+++
@@ -1,4 +0,0 @@
-from igramscraper.two_step_verification.two_step_verification_abstract_class import TwoStepVerificationAbstractClass
-from igramscraper.two_step_verification.console_verification import ConsoleVerification
-
-__all__ = ["TwoStepVerificationAbstractClass", "ConsoleVerification"]
